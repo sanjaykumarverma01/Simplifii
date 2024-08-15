aa6723351e17c33409dad4b8fcd54840d1fb905a
SK-WebDeveloper <vermasanjaykumar97@gmail.com>
2024-08-16 00:30:19 +0530

    Access client from server and setup for deployment

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ const { connectDB } = require("./config/db");
 const { notFound, errorHandler } = require("./middlewares/errorMiddleware");
 const userRoute = require("./routes/user.routes");
 const cors = require("cors");
+const path = require("path");
 
 dotenv.config();
 connectDB();
@@ -13,6 +14,23 @@ app.use(express.json()); //  to accept json data
 
 app.use(cors());
 app.use("/api/user", userRoute);
+
+// --------------------------deployment------------------------------
+
+const __dirname1 = path.resolve(__dirname, "..");
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname1, "client/build")));
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname1, "client", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running..");
+  });
+}
+
+// --------------------------deployment------------------------------
 
 // error handling
 app.use(notFound);
