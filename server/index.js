24134f6d27329a94d0b48e0ef839d7247eabef38
SK-WebDeveloper <vermasanjaykumar97@gmail.com>
2024-08-16 00:53:29 +0530

    Modify deployment

--- server/index.js
+++ server/index.js
@@ -17,10 +17,11 @@ app.use("/api/user", userRoute);
 
 // --------------------------deployment------------------------------
 
-const __dirname1 = path.resolve(__dirname, "..");
+const __dirname1 = path.resolve();
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname1, "client/build")));
+  
   app.get("*", (req, res) =>
     res.sendFile(path.resolve(__dirname1, "client", "build", "index.html"))
   );
